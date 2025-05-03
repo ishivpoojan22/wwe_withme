5f402ce10f9cd3a5bbd3acfdf0ffb8252cc9fffc
shiv <myselfsp22@gmail.com>
2025-05-03 20:01:48 +0530

    uploading files

---
+++ src/data.js
@@ -0,0 +1,67 @@
+const data = [
+	{
+		id: 1,
+		name: "John Cena",
+		info: "John Cena is a WWE superstar, actor, and philanthropist. A 16-time world champion, he’s known for his 'Never Give Up' motto. Cena transitioned to acting, starring in films like Bumblebee and F9, and TV series Peacemaker. He’s also a prominent supporter of the Make-A-Wish Foundation.",
+		image: "https://i.pinimg.com/474x/c6/b7/32/c6b73204af1f7c416cd9eda8b350d14e.jpg",
+		price: "80 million",
+	},
+	{
+		id: 2,
+		name: "Roman Reigns",
+		info: "Roman Reigns is a WWE superstar and a multi-time world champion, known for his dominant 'Tribal Chief' persona. He is a member of the Anoa'i wrestling family. Reigns has headlined numerous major events, including WrestleMania, and is celebrated for his intense in-ring style and captivating character work.",
+		image: "https://i.pinimg.com/474x/fe/6d/15/fe6d15444cd7a6d43bd9110ff5ee48a5.jpg",
+		price: "$14 million",
+	},
+	{
+		id: 3,
+		name: "Randy Orton",
+		info: "Randy Orton is a 14-time WWE World Champion, known for his cold, calculated persona and devastating 'RKO' finisher. A third-generation wrestler, Orton rose to fame in the early 2000s and remains a major WWE star, known for his in-ring ability, longevity, and involvement in multiple iconic storylines.",
+		image: "https://i.pinimg.com/474x/5d/9b/4d/5d9b4df7c03e23cbb952cda6800006db.jpg",
+		price: "$7 million",
+	},
+	{
+		id: 4,
+		name: "CM Punk",
+		info: "CM Punk is a former WWE Champion known for his rebellious 'pipebomb' promos and outspoken attitude. He became one of WWE's most popular stars in the late 2000s and transitioned to MMA, competing in the UFC. Punk’s influence extended beyond wrestling into pop culture and combat sports.",
+		image: "https://i.pinimg.com/474x/f2/6d/6e/f26d6eb228395e551c4cb6cd25be0525.jpg",
+		price: "$12 million",
+	},
+	{
+		id: 5,
+		name: "The Rock",
+		info: "The Rock, also known as Dwayne Johnson, is a 10-time WWE Champion and one of the biggest stars in professional wrestling. He transitioned to Hollywood, becoming a global superstar in films like Fast & Furious and Jumanji. Known for his charisma, The Rock remains a cultural icon.",
+		image: "https://i.pinimg.com/474x/96/02/c8/9602c81efa01b2ca465f91d0f69c51dc.jpg",
+		price: "$800 million",
+	},
+	{
+		id: 6,
+		name: "Triple H",
+		info: "Triple H, a 14-time world champion, is a WWE legend known for his intense in-ring style and leadership. He was a key figure in the 'Attitude Era' and co-founded Evolution. As an executive, he helped shape WWE’s future through the creation of NXT, nurturing new talent.",
+		image: "https://i.pinimg.com/474x/c0/00/6a/c0006a4e8ccd24ce14a2304cbf73377f.jpg",
+		price: "$250 million",
+	},
+	{
+		id: 7,
+		name: "Undertaker",
+		info: "The Undertaker is a WWE legend known for his iconic 'Deadman' persona and unmatched legacy. Famous for his undefeated streak at WrestleMania, he became a symbol of dominance, mystery, and endurance. With a career spanning over three decades, The Undertaker is considered one of the greatest wrestlers of all time.",
+		image: "https://i.pinimg.com/474x/47/6d/02/476d0281950300e21583dfc2be6908db.jpg",
+		price: "$17 million",
+	},
+    {
+		id: 7,
+		name: "Goldberg",
+		info: "Goldberg is a WWE and WCW legend known for his explosive style and dominant 'undefeated streak' in WCW. A multi-time world champion, his power-based wrestling and iconic spear finisher made him a fan favorite. Goldberg is also remembered for his short, intense matches and his global impact on wrestling.",
+		image: "https://i.pinimg.com/474x/10/7a/0e/107a0e87178653f21407ee9a9963b92d.jpg",
+		price: " $16 million",
+	},
+    {
+		id: 7,
+		name: "Khali",
+		info: "The Great Khali is a WWE legend known for his towering 7'1 frame and immense strength. As the World Heavyweight Champion, he became a dominant force in the ring. Khali's size and power made him a unique presence in WWE, where he was a memorable figure in the mid-2000s.",
+		image: "https://i.pinimg.com/474x/f6/33/e2/f633e263a43011359147b782133b99fd.jpg",
+		price: "$8 million",
+	},
+];
+
+export default data;
