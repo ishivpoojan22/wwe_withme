a7abcf8e27d45c7fe8072db0e5fa5b686e40a8d8
shiv <myselfsp22gmail.com>
2025-05-06 09:30:02 +0530

    working on app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React, { useState } from "react";
+import data from "./data";
+import Players from './Components/Players';
+import Refresh from './Components/Refresh';
 
-import './App.css';
+const App = () => {
+  const [player, setPlayer] = useState(data);
 
-function App() {
+  function removePlayer(id) {
+    const newPlayer = player.filter(player => player.id !== id)
+    console.log(id);
+    setPlayer(newPlayer);
+  }
+
+
+  if (player.length === 0) {
+    return <Refresh setPlayer={setPlayer}  data={data}/>
+  }
+
   return (
-    <div className="App">
-       <div>wwe</div>
-    </div>
-  );
-}
+    <Players players={player} removePlayer={removePlayer} />
+  )
+};
 
 export default App;
